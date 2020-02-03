439e7993321e8eb29c112e92252a8f1f93cfacf7
Roy van de Water <roy.vwater@gmail.com>
2020-02-03 13:24:43 -0700

    Game logic is complete

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { INVALID_MOVE } from 'boardgame.io/core';
+import { Client } from 'boardgame.io/react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const randomDoorIndex = doors => Math.floor(Math.random() * doors.length)
+
+const initializeDoors = () => {
+  const doors = Array(3).fill({}).map(() => ({contains: 'goat', isOpen: false, chosen: false}))
+  doors[randomDoorIndex(doors)].contains = "car"
+  return doors;
 }
+
+const nonChosenGoatDoors = doors => doors.filter(door => !door.chosen && door.contains === 'goat')
+
+const MontyHall = {
+  setup: () => ({ 
+    doors: initializeDoors(),
+  }),
+
+  phases: {
+    initialChoice: {
+      start: true,
+      moves: {
+        clickDoor: (G, ctx, id) => {
+          G.doors[id].chosen = true
+          ctx.events.endPhase();
+        },
+      },
+      next: 'finalChoice',
+    },
+    finalChoice: {
+      onBegin: G => {
+        const doors = nonChosenGoatDoors(G.doors)
+        doors[randomDoorIndex(doors)].isOpen = true
+      },
+      moves: {
+        clickDoor: (G, ctx, id) => {
+          if (G.doors[id].isOpen) return INVALID_MOVE;
+
+          G.doors.forEach(door => door.chosen = false)
+          G.doors[id].chosen = true
+          G.doors[id].isOpen = true
+          ctx.events.endGame();
+        }
+      }
+    }
+  },
+};
+
+const App = Client({ game: MontyHall, numPlayers: 1 });
 
 export default App;
