c9da538f7d9d4f323bda151aa59376449fd8e967
Roy van de Water <roy.vwater@gmail.com>
2020-02-04 12:13:01 -0700

    Vaguelly support mobile

--- src/Board.jsx
+++ src/Board.jsx
@@ -12,14 +12,21 @@ const Door = ({chosen, contents, isOpen, onClick}) => {
   return <div className="door closed-door" onClick={onClick} />
 }
 
+const gameMessage = (phase, gameover) =>
+  {
+    if (gameover) return `You ${gameover.winner ? 'Win' : 'Lose'}!`;
+    if (phase === 'finalChoice') return 'Keep your door or switch?';
+    return 'Pick a door';
+  }
+
 const Board = (props) => {
   const doors = props.G.doors;
   const chooseDoor = props.moves.chooseDoor
-  const gameover = props.ctx.gameover
+  const { gameover, phase } = props.ctx
 
   return (
     <main className="board">
-      <h1 className="game-over-message">{gameover && `You ${gameover.winner ? 'Win' : 'Lose'}!`}&nbsp;</h1>
+      <h1 className="game-message">{gameMessage(phase, gameover)}&nbsp;</h1>
       <div className="doors">
         {doors.map((door, i) => <Door key={i} {...door} onClick={() => chooseDoor(i)} />)}
       </div>
